d991eac857a6b9f1e07392d6e85a1efd202ff04e
Burk Price <webigrator@gmail.com>
2019-09-06 03:18:17 -0600

    Mouse event decorators grouped in mouse

---
+++ src/decorator/mouse/ScrollElementDecorator.ts
@@ -0,0 +1,21 @@
+import {Decorator} from "../../Core";
+
+class ScrollElementDecorator extends Decorator<any> {
+
+	public wire(): void {
+		this.consume("scroll");
+		this.listenTo("dom", "scroll", this.handleScroll);
+	}
+
+	public unwire(): void {
+		// Intentionally do nothing
+	}
+
+	public handleScroll(event: Event): void {
+		this.getMediator().invoke(event);
+		this.notifyModelInteraction();
+	}
+
+}
+
+export default ScrollElementDecorator;
---
+++ src/decorator/mouse/WheelElementDecorator.ts
@@ -0,0 +1,21 @@
+import {Decorator} from "../../Core";
+
+class WheelElementDecorator extends Decorator<any> {
+
+	public wire(): void {
+		this.consume("wheel");
+		this.listenTo("dom", "wheel", this.handleWheel);
+	}
+
+	public unwire(): void {
+		// Intentionally do nothing
+	}
+
+	public handleWheel(event: Event): void {
+		this.getMediator().invoke(event);
+		this.notifyModelInteraction();
+	}
+
+}
+
+export default WheelElementDecorator;
