85b7b147d3d74040c049bb93ec4120595b9d3c14
Matthew Werny <matthew.werny@unboundedprime.com>
2019-12-16 16:01:16 -0700

    First portion of guard code

---
+++ src/ComponentConfig.spec.ts
@@ -0,0 +1,58 @@
+import { assert } from "chai";
+import { describe, it, xit } from "mocha";
+import { ComponentConfig, ComponentConfigBuilder } from "./ComponentConfig";
+
+describe("ComponentConfig tests", () => {
+
+	it("ComponentConfigBuild values", () => {
+		const instance: ComponentConfig = new ComponentConfigBuilder()
+			.withPrefix("custom-prefix")
+			.withAttribute("first")
+			.withAttribute("second")
+			.withMetadata("alpha", "one")
+			.withMetadata("beta", "two")
+			.withMetadata("gamma", "three")
+			.build();
+
+		assert.isNotNull(instance, "is null");
+		assert.equal(instance.getPrefix(), "custom-prefix", "must have custom prefix");
+		assert.equal(instance.getAttributes().length, 2, "attributes must have 2 elements");
+		assert.equal(instance.getAttributes()[0], "first", "attribute 0 must be 'first'");
+		assert.equal(instance.getAttributes()[1], "second", "attribute 1 must be 'second'");
+		assert.equal(instance.getMetadata("alpha"), "one", "metadata value alpha must be 'one'");
+		assert.equal(instance.getMetadata("beta"), "two", "metadata value beta must be 'two'");
+		assert.equal(instance.getMetadata("gamma"), "three", "metadata value gamma must be 'three'");
+		assert.isNull(instance.getMetadata("bogus"), "metadata value bogus must be null");
+	});
+
+	function assertNullGuarded(expected: string, activity: () => void) {
+		let thrown: Error = null;
+
+		try {
+			activity();
+		} catch (e) {
+			thrown = e;
+		}
+
+		assert.isNotNull(thrown, "error must be thrown");
+		assert.equal(thrown.name, "NullValueError", "must have correct name");
+		assert.equal(thrown.message, expected + " shall not be null", "must have correct message");
+	}
+
+	it("ComponentConfigBuild withPrefix(null)", () => {
+		assertNullGuarded("prefix", () => new ComponentConfigBuilder().withPrefix(null));
+	});
+
+	it("ComponentConfigBuild withAttribute(null)", () => {
+		assertNullGuarded("name", () => new ComponentConfigBuilder().withAttribute(null));
+	});
+
+	it("ComponentConfigBuild withMetadata(null, nonNull)", () => {
+		assertNullGuarded("name", () => new ComponentConfigBuilder().withMetadata(null, "value"));
+	});
+
+	it("ComponentConfigBuild withMetadata(nonNull, null)", () => {
+		assertNullGuarded("value", () => new ComponentConfigBuilder().withMetadata("name", null));
+	});
+
+});
