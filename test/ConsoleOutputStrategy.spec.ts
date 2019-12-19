3297490a4524bb140fc92ab2270937c4c3e3e2f2
Burk Price <webigrator@gmail.com>
2019-12-18 19:03:38 -0700

    Updated and new unit tests

--- test/ConsoleOutputStrategy.spec.ts
+++ test/ConsoleOutputStrategy.spec.ts
@@ -1,46 +1,23 @@
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import { describe, it, xit } from "mocha";
 import ConsoleOutputStrategy from "../src/logger/ConsoleOutputStrategy";
 import Level from "../src/logger/Level";
-import Logger from "../src/logger/Logger";
-import LoggerFactory from "../src/logger/LoggerFactory";
+import LevelUtils from "../src/logger/LevelUtils";
+import OutputStrategy from "../src/logger/OutputStrategy";
 
 describe("ConsoleOutputStrategy tests", () => {
 
-	const msg: string = "Testing message ---";
-	const lgr: Logger = LoggerFactory.getLogger("ConsoleOutputStrategy.spec");
+	const msg: string = "test payload";
 	const cos: ConsoleOutputStrategy = new ConsoleOutputStrategy();
 
-	it(".log(logger, Level.WARN, msg)", () => {
-		cos.log(lgr, Level.WARN, msg + "--- WARN");
+	it("ConsoleOutputStrategy is not null", () => {
+		expect(cos).to.not.be.null;
 	});
 
-	it(".log(logger, Level.TRACE, msg, true)", () => {
-		cos.log(lgr, Level.TRACE, msg, true);
-	});
-
-	it(".log(logger, Level.TRACE, msg, false)", () => {
-		cos.log(lgr, Level.TRACE, msg, false);
-	});
-
-	it(".log(logger, Level.TRACE, msg) - implied false", () => {
-		cos.log(lgr, Level.TRACE, msg);
-	});
-
-	it(".log(logger, Level.ERROR, msg, error)", () => {
-		cos.log(lgr, Level.ERROR, msg, new Error("Working test error"));
-	});
-
-	it(".log(logger, Level.ERROR, error)", () => {
-		cos.log(lgr, Level.ERROR, new Error("Working test error"));
-	});
-
-	it(".log(logger, Level.DEBUG, msg)", () => {
-		cos.log(lgr, Level.DEBUG, msg + "--- DEBUG");
-	});
-
-	it(".log(logger, Level.INFO, msg)", () => {
-		cos.log(lgr, Level.INFO, msg + "--- INFO");
+	it("ConsoleOutputStrategy log()", () => {
+		LevelUtils.values().forEach((l: Level) => {
+			cos.log("ConsoleOutputStrategySpec", l, msg + " *** " + LevelUtils.stringValueOf(l));
+		});
 	});
 
 });
---
+++ test/LoggerServiceImpl.spec.ts
@@ -0,0 +1,45 @@
+import { assert } from "chai";
+import { describe, it, xit } from "mocha";
+import Level from "../src/logger/Level";
+import LoggerService from "../src/logger/LoggerService";
+import LoggerServiceImpl from "../src/logger/LoggerServiceImpl";
+
+describe("LogggerServiceImpl tests", () => {
+	const ls: LoggerServiceImpl = LoggerServiceImpl.INSTANCE;
+
+	it("LoggerService setLevel - TRACE", () => {
+		ls.setLevel(Level.TRACE);
+		assert.isTrue(ls.isTrace());
+	});
+
+	it("LoggerService setLevel - DEBUG", () => {
+		ls.setLevel(Level.DEBUG);
+		assert.isTrue(ls.isDebug());
+	});
+
+	it("LoggerService setLevel - INFO", () => {
+		ls.setLevel(Level.INFO);
+		assert.isTrue(ls.isInfo());
+	});
+
+	it("LoggerService setLevel - WARN", () => {
+		ls.setLevel(Level.WARN);
+		assert.isTrue(ls.isWarn());
+	});
+
+	it("LoggerService setLevel - ERROR", () => {
+		ls.setLevel(Level.ERROR);
+		assert.isTrue(ls.isError());
+	});
+
+	it("LoggerService setLevel - FATAL", () => {
+		ls.setLevel(Level.FATAL);
+		assert.isTrue(ls.isFatal());
+	});
+
+	it("LoggerService setLevel - DISABLED", () => {
+		ls.setLevel(Level.DISABLED);
+		assert.isTrue(ls.isDisabled());
+	});
+
+});
