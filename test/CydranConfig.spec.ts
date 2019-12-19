d611a3842f93228ddceea080058b1310726625b3
Burk Price <webigrator@gmail.com>
2019-12-18 19:34:13 -0700

    Unit test for CydranConfig

--- test/CydranConfig.spec.ts
+++ test/CydranConfig.spec.ts
@@ -1,12 +1,62 @@
 import { assert } from "chai";
 import { describe, it, xit } from "mocha";
 import CydranConfig from "../src/CydranConfig";
+import LoggerServiceImpl from "../src/logger/LoggerServiceImpl";
 
 describe("CydranConfig tests", () => {
 
 	it("new CydranConfig()", () => {
 		const instance: CydranConfig = new CydranConfig();
 		assert.isNotNull(instance, "is null");
+	});
+
+	it("useTrace()", () => {
+		const cc: CydranConfig = new CydranConfig();
+		cc.useTrace();
+		const ls = LoggerServiceImpl.INSTANCE;
+		assert.isTrue(ls.isTrace());
+	});
+
+	it("useDebug()", () => {
+		const cc: CydranConfig = new CydranConfig();
+		cc.useDebug();
+		const ls = LoggerServiceImpl.INSTANCE;
+		assert.isTrue(ls.isDebug());
+	});
+
+	it("useInfo()", () => {
+		const cc: CydranConfig = new CydranConfig();
+		cc.useInfo();
+		const ls = LoggerServiceImpl.INSTANCE;
+		assert.isTrue(ls.isInfo());
+	});
+
+	it("useWarn()", () => {
+		const cc: CydranConfig = new CydranConfig();
+		cc.useWarn();
+		const ls = LoggerServiceImpl.INSTANCE;
+		assert.isTrue(ls.isWarn());
+	});
+
+	it("useError()", () => {
+		const cc: CydranConfig = new CydranConfig();
+		cc.useError();
+		const ls = LoggerServiceImpl.INSTANCE;
+		assert.isTrue(ls.isError());
+	});
+
+	it("useFatal()", () => {
+		const cc: CydranConfig = new CydranConfig();
+		cc.useFatal();
+		const ls = LoggerServiceImpl.INSTANCE;
+		assert.isTrue(ls.isFatal());
+	});
+
+	it("useDisabled()", () => {
+		const cc: CydranConfig = new CydranConfig();
+		cc.useDisabled();
+		const ls = LoggerServiceImpl.INSTANCE;
+		assert.isTrue(ls.isDisabled());
 	});
 
 });
