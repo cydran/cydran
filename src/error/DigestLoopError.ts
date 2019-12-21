fbd6694a90eb92d65baa37f0ec53c37b6d2c88de
Burk Price <webigrator@gmail.com>
2019-12-21 03:48:50 -0700

    Unnecessary guard code
    if condition were true then throwing an instance of it would be null

--- src/error/DigestLoopError.ts
+++ src/error/DigestLoopError.ts
@@ -3,7 +3,6 @@ import { CydranError, NEWI } from "./AbstractCydranError";
 class DigestLoopError extends CydranError {
 
 	constructor(msg: string, reps?: any) {
-		if (!new.target) { throw (new.target + NEWI); }
 		super(msg, reps);
 	}
 
