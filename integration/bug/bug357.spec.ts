9a8c2da796a781130f13df1b766fc535dca96b9a
Matthew Werny <matthew.werny@unboundedprime.com>
2020-05-24 00:14:25 -0600

    Forced object to be treated as Window

--- integration/bug/bug357.spec.ts
+++ integration/bug/bug357.spec.ts
@@ -1,6 +1,6 @@
 import { JSDOM } from "jsdom";
 const WIN = new JSDOM("<html><body></body></html>").window;
-global["window"] = WIN as unknown;
+global["window"] = WIN as unknown as Window;
 
 import { assert, expect } from "chai";
 import { describe, it, xit } from "mocha";
