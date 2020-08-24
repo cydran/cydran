6c16601c7972578d0008782f9f884e4b4393014a
Burk Price <webigrator@gmail.com>
2020-08-23 23:35:17 -0700

    updated integration tests

--- integration/bug/bug481.integration-spec.ts
+++ integration/bug/bug481.integration-spec.ts
@@ -8,19 +8,14 @@ interface Item {
 
 }
 
-class TestComponent extends Component {
+abstract class AbstractTestComponent extends Component {
 
-	private items: Item[];
+	protected items: Item[];
 
-	private values: string[];
+	protected values: string[];
 
-	constructor() {
-		super(`
-			<div>
-				<select c:each="m().items" c:each:mode="generated" c:model="m().values" size="10" c:id="specimen-element" multiple>
-				</select>
-			</div>
-		`);
+	constructor(template) {
+		super(template);
 
 		this.values = ["2", "4"];
 
@@ -46,7 +41,55 @@ class TestComponent extends Component {
 
 }
 
-test("Template error should be thrown if <template> node does not exist in a Cydran 'each' context", () => {
+const TEMPLATE1 = `<div>
+				<select c:each="m().items" c:each:mode="generated" c:model="m().values" size="10" c:id="specimen-element" multiple>
+				</select>
+			</div>`;
+const TEMPLATE2 = `<div>
+				<select c:each="m().items" c:each:mode="generated" c:model="m().values" size="10" c:id="specimen-element" multiple>
+					<template c:type="item">
+						<div></div>
+					</template>
+				</select>
+			</div>`;
+const TEMPLATE3 = `<div>
+				<select c:each="m().items" c:each:mode="generated" c:model="m().values" size="10" c:id="specimen-element" multiple>
+					<div>
+						<template c:type="item"></template>
+					</div>
+				</select>
+			</div>`;
+const TEMPLATE4 = `<div>
+				<select c:each="m().items" c:each:mode="generated" c:model="m().values" size="10" c:id="specimen-element" multiple>
+					<template></template>
+				</select>
+			</div>`;
+
+class TestComponent1 extends AbstractTestComponent {
+	constructor() {
+		super(TEMPLATE1);
+	}
+}
+
+class TestComponent2 extends AbstractTestComponent {
+	constructor() {
+		super(TEMPLATE2);
+	}
+}
+
+class TestComponent3 extends AbstractTestComponent {
+	constructor() {
+		super(TEMPLATE3);
+	}
+}
+
+class TestComponent4 extends AbstractTestComponent {
+	constructor() {
+		super(TEMPLATE4);
+	}
+}
+
+test("TemplateError SHOULD be thrown if <template c:type='item'> node does not exist in a Cydran 'each' context", () => {
 	document.body.innerHTML = '<div id="app">first</div>';
 
 	let thrown: Error = null;
@@ -55,7 +98,7 @@ test("Template error should be thrown if <template> node does not exist in a Cyd
 		builder("body")
 			.withInfoLogging()
 			.withInitializer((stage: Stage) => {
-				const component: TestComponent = new TestComponent();
+				const component: TestComponent1 = new TestComponent1();
 				stage.setComponent(component);
 			})
 			.build()
@@ -67,4 +110,68 @@ test("Template error should be thrown if <template> node does not exist in a Cyd
 	expect(thrown).not.toBeNull();
 	expect(thrown.name).toEqual("TemplateError");
 	expect(thrown.message).toEqual("SELECT element with a c:each attribute must have at least one child <template> node/element.");
+});
+
+test("TemplateError should NOT be thrown if <template c:type='item'> node DOES exist in a Cydran 'each' context", () => {
+	document.body.innerHTML = '<div id="app">first</div>';
+
+	let thrown: Error = null;
+
+	try {
+		builder("body")
+			.withInfoLogging()
+			.withInitializer((stage: Stage) => {
+				const component: TestComponent2 = new TestComponent2();
+				stage.setComponent(component);
+			})
+			.build()
+			.start();
+	} catch (e) {
+		thrown = e;
+	}
+	expect(thrown == null);
+});
+
+test("ModuleAffinityError SHOULD be thrown if <template> tag exists but not as a root child in a Cydran 'each' context", () => {
+	document.body.innerHTML = '<div id="app">first</div>';
+
+	let thrown: Error = null;
+
+	try {
+		builder("body")
+			.withInfoLogging()
+			.withInitializer((stage: Stage) => {
+				const component: TestComponent3 = new TestComponent3();
+				stage.setComponent(component);
+			})
+			.build()
+			.start();
+	} catch (e) {
+		thrown = e;
+	}
+
+	expect(thrown).not.toBeNull();
+	expect(thrown.name).toEqual("ModuleAffinityError");
+});
+
+test("ModuleAffinityError SHOULD be thrown if <template> tag exists but without proper attributes in a Cydran 'each' context", () => {
+	document.body.innerHTML = '<div id="app">first</div>';
+
+	let thrown: Error = null;
+
+	try {
+		builder("body")
+			.withInfoLogging()
+			.withInitializer((stage: Stage) => {
+				const component: TestComponent4 = new TestComponent4();
+				stage.setComponent(component);
+			})
+			.build()
+			.start();
+	} catch (e) {
+		thrown = e;
+	}
+
+	expect(thrown).not.toBeNull();
+	expect(thrown.name).toEqual("ModuleAffinityError");
 });
