fcaf1bad79c84cd7e38a4320cec2745114b1a723
Burk Price <webigrator@gmail.com>
2020-09-13 00:01:20 -0600

    removal of unnecessary code elements

--- integration/bug/bug492.integration-spec.ts
+++ integration/bug/bug492.integration-spec.ts
@@ -1,64 +1,22 @@
 import { builder, Component } from 'cydran';
 
-class ParentComponent extends Component {
-
-	private value: {
-		first: string;
-	};
-
-	constructor() {
-		super(`<div><script type="cydran/region" c:name="body" c:value="m().value"></script></div>`);
-		this.value = {
-			first: "Some Value"
-		};
-	}
-
-	public update(): void {
-		this.$apply(() => {
-			this.value.first = "Some other value";
-		});
-	}
-
-}
-
-class ChildComponent extends Component {
-
-	private modelValue: string;
-
-	private items: any[];
-
-	constructor() {
-		super(`<div><ul c:each="m().items" c:each:mode="generated"><template c:type="empty"><li>{{m().modelValue}}<br />{{v().first}}</li></template><template c:type="item"><div>text</div></template></ul></div>`);
-		this.items = [];
-		this.modelValue = "Some model value";
-	}
-
-	public update(): void {
-		this.$apply(() => {
-			this.modelValue = "Some other model value";
-		});
-	}
-
-}
-
-test("v() or m() should be proxied for IMPLICIT component", () => {
+test("set production mode to true", () => {
 	document.body.innerHTML = '<div id="app"></div>';
 
-	builder("#app")
-		.build()
-		.withInitializer((stage: Stage) => {
-			const childComponent: ChildComponent = new ChildComponent();
-			const parentComponent: ParentComponent = new ParentComponent();
-			parentComponent.setChild("body", childComponent);
-			stage.setComponent(parentComponent);
-
-			expect(document.body.innerHTML).toEqual(`<div id="app"><div><div><ul><li><!--#-->Some model value<!--#--><br><!--#-->Some Value<!--#--></li></ul></div></div></div>`);
-
-			parentComponent.update();
-			childComponent.update();
-
-			expect(document.body.innerHTML).toEqual(`<div id="app"><div><div><ul><li><!--#-->Some other model value<!--#--><br><!--#-->Some other value<!--#--></li></ul></div></div></div>`);
+	const stage: Stage = builder("#app")
+		.withProperties({
+			"cydran.production.enabled": true
 		})
-		.start();
+		.build();
+	stage.start();
+	expect(stage.getProperties().isTruthy('cydran.production.enabled')).toEqual(true);
+});
 
+test("expect default mode to be development (production mode == false)", () => {
+	document.body.innerHTML = '<div id="app"></div>';
+
+	const stage: Stage = builder("#app")
+		.build();
+	stage.start();
+	expect(stage.getProperties().isTruthy('cydran.production.enabled')).toEqual(false);
 });
