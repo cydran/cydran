17702bbe3034c052a4aff291e55e629581d50577
Burk Price <webigrator@gmail.com>
2020-11-20 00:34:55 -0700

    beginnings of Each unit testing

--- test/Component.Each.spec.ts
+++ test/Component.Each.spec.ts
@@ -1,13 +1,63 @@
-import { Component } from "@/Component";
-import { anything, instance, mock, spy, verify, when } from "ts-mockito";
+import { ElementMediatorDependencies } from "@/Interfaces";
+import { assertNoErrorThrown, assertNullGuarded } from "@/TestUtils";
+import { Component, Each, MvvmImpl } from "@/Component";
+import { spy, verify, mock } from "ts-mockito";
+import { ComponentOptions, Module, OnContinuation } from '@/Interfaces';
+import { JSDOM } from "jsdom";
 
-const testParentComponent: Component = instance(mock(Component));
-const testParentId: string = "10-112-22";
-const testTemplate: string = "<div></div>";
 const testPrefix: string = "prefix";
-const testModelFn = () => { /**/ };
-const testItem = {};
+const testModelFn: Function = () => { /**/ };
+const testItem: Object = {};
 
-test.skip("Each constructor", () => {
-	//
+const doc = new JSDOM(`
+	<div id="whack" c:each="m().data" c:each:mode="none">
+		<template c:type="item><div/></template>
+		<script type="cydran/region" c:name="xyz"></script>
+	</div>
+	`).window.document;
+
+class SimpleComponent extends Component {
+	constructor(template: string, options?: ComponentOptions) {
+		super(template, options);
+	}
+}
+
+const dependencies: ElementMediatorDependencies = {
+	el: doc.querySelector("div"),
+	expression: "true",
+	model: {'data': [
+		{'id': 0, 'name': 'zero'},
+		{'id': 1, 'name': 'one'},
+		{'id': 2, 'name': 'two'},
+		{'id': 3, 'name': 'three'},
+		{'id': 4, 'name': 'four'}
+	]
+	},
+	mvvm: mock(MvvmImpl),
+	parent: null,
+	prefix: "prefix",
+	module: null
+};
+
+test("Each construtor - null dependencies", () => {
+	assertNullGuarded("dependencies", () => new Each(null));
+});
+
+test("Each construtor - with dependencies", () => {
+	const emed = new Each(dependencies);
+	expect(emed).not.toBeNull();
+});
+
+test.skip("wire()", () => {
+	const emed = new Each(dependencies);
+	expect(emed).not.toBeNull();
+	emed.wire();
+});
+
+test("unwire()", () => {
+	const emed = new Each(dependencies);
+	const spyemed = spy(emed);
+	expect(emed).not.toBeNull();
+	emed.unwire();
+	verify(spyemed.unwire()).once();
 });
