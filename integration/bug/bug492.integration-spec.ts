f98912695d3cd397110fabb79c0bbf053efe32c8
Matthew Werny <matthewwerny+github@gmail.com>
2020-12-10 00:54:58 -0700

    First refactor to introduce state machine support (#512)
    * Checkpoint commit
    
    * Checkpoint commit
    
    * Checkpoint commit
    
    * Checkpoint commit
    
    * Checkpoint commit
    
    * Initial refactor to bring in state machine

--- integration/bug/bug492.integration-spec.ts
+++ integration/bug/bug492.integration-spec.ts
@@ -1,6 +1,8 @@
-import { builder, Component } from 'cydran';
+import { builder, reset } from 'cydran';
 
 test("set production mode to true", () => {
+	reset();
+
 	document.body.innerHTML = '<div id="app"></div>';
 
 	const stage: Stage = builder("#app")
@@ -9,14 +11,18 @@ test("set production mode to true", () => {
 		})
 		.build();
 	stage.start();
+
 	expect(stage.getProperties().isTruthy('cydran.production.enabled')).toEqual(true);
 });
 
 test("expect default mode to be development (production mode == false)", () => {
+	reset();
+
 	document.body.innerHTML = '<div id="app"></div>';
 
 	const stage: Stage = builder("#app")
 		.build();
 	stage.start();
+
 	expect(stage.getProperties().isTruthy('cydran.production.enabled')).toEqual(false);
 });
