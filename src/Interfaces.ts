a469f152597fead6495716d69bdd6aa4f514abde
Burk Price <webigrator@gmail.com>
2020-12-12 00:36:44 -0700

    pubsub

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -123,58 +123,6 @@ interface Supplier<T> {
 
 }
 
-interface Broker extends Disposable {
-
-	broadcast(channelName: string, messageName: string, payload?: any): void;
-
-	addListener(listener: Listener): void;
-
-	removeListener(listener: Listener): void;
-
-}
-
-interface OnContinuation {
-
-	invoke(target: (payload: any) => void): void;
-
-	forChannel(name: string): ForChannelContinuation;
-
-}
-
-interface ForChannelContinuation {
-
-	invoke(target: (payload: any) => void): void;
-
-}
-
-interface Listener extends Disposable {
-
-	register(messageName: string, fn: (payload: any) => void): void;
-
-	receive(messageName: string, payload: any): void;
-
-	getChannelName(): string;
-
-}
-
-interface PubSub extends Disposable {
-
-	message(channelName: string, messageName: string, payload?: any): void;
-
-	broadcast(channelName: string, messageName: string, payload?: any): void;
-
-	broadcastGlobally(channelName: string, messageName: string, payload?: any): void;
-
-	on(messageName: string): OnContinuation;
-
-	enableGlobal(): void;
-
-	disableGlobal(): void;
-
-	isGlobalEnabled(): boolean;
-
-}
-
 interface Scope {
 
 	add(name: string, item: any): void;
