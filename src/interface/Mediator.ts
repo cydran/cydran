0b5bf52b84cb532e10dbfbdffb7e8ae8c2bfd750
Burk Price <webigrator@gmail.com>
2020-12-12 00:37:01 -0700

    moved ElementMediator

--- src/interface/Mediator.ts
+++ src/interface/Mediator.ts
@@ -20,7 +20,35 @@ interface ModelMediator<T> extends Disposable, DigestionCandidate {
 	populate(): void;
 }
 
+interface ElementMediator<M, E extends HTMLElement | Text, P>
+	extends Disposable, MediatorSource, Tellable {
+	/**
+	 * [message description]
+	 * @param {string} channelName [description]
+	 * @param {string} messageName [description]
+	 * @param {any}    payload     [description]
+	 */
+	message(channelName: string, messageName: string, payload?: any): void;
+
+	is(name: string): boolean;
+
+	onInit(): void;
+
+	onPopulate(): void;
+
+	onMount(): void;
+
+	onUnmount(): void;
+
+	onDispose(): void;
+
+	onValidate(el: E, fn: (name: string, value?: any) => Validators): void;
+
+	onNestingChanged(): void;
+}
+
 export {
+	ElementMediator,
 	MediatorSource,
 	ModelMediator
 };
