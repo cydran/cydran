59c62c6c386e7e617b45fa5e191ee130c04e934d
Burk Price <webigrator@gmail.com>
2021-01-05 23:47:22 -0700

    updated build

--- esbuild.js
+++ esbuild.js
@@ -1,26 +1,39 @@
 const esb = require("esbuild");
+const fs = require("fs-extra");
+const path = require("path");
 const libName = "cydran";
 const srcDir = "src";
 const targDir = "dist";
 
 let minify = false;
+let incBuild = false;
 if(process.env.min) {
 	minify = !!process.env.min;
 }
+if(process.env.inc) {
+	incBuild = !!process.env.inc;
+}
+
+const copyBanner = fs.readFileSync(path.join(__dirname, "src/cydran_copyright.txt"), "utf8");
 
 const doBuild = (doMin) => {
 	esb
-	.build({
-		entryPoints: [`${srcDir}/index.ts`],
-		outfile: `${targDir}/${libName}.${doMin ? "min." : ""}js`,
-		globalName: libName,
-		minify: doMin,
-		bundle: true,
-		sourcemap: true,
-		platform: "browser",
-		format: "iife"
-	})
-	.catch(() => process.exit(1));
+		.build({
+			charset: "utf8",
+			entryPoints: [`${srcDir}/index.ts`],
+			banner: copyBanner,
+			outfile: `${targDir}/${libName}.${doMin ? "min." : ""}js`,
+			globalName: libName,
+			minify: doMin,
+			bundle: true,
+			sourcemap: true,
+			platform: "browser",
+			format: "iife",
+			incremental: incBuild,
+			logLevel: "info",
+			color: true,
+		})
+		.catch(() => process.exit(1));
 };
 
 doBuild(false);
