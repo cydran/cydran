b78c9c124dc0b833c29e57851a900b37c13e6563
Matthew Werny <matthewwerny+github@gmail.com>
2021-01-18 00:58:58 -0700

    Added AMD module support

--- esbuild.js
+++ esbuild.js
@@ -19,26 +19,27 @@ if (process.env.inc) {
 const copyBanner =
 	`/*\n${process.env.npm_package_version}\n---\n${fs.readFileSync(path.join(__dirname, "src/cydran_copyright.txt"), "utf8")}\n*/`;
 
+const footer = `if (define !== null && define !== undefined && typeof define === "function" && define.amd) define([], function() { return cydran; });`;
+
 const doBuild = (doMin) => {
-	esb
-		.build({
-			charset: "utf8",
-			entryPoints: [`${srcDir}/index.js`],
-			target: "es5",
-			banner: copyBanner,
-			outfile: `${targDir}/${libName}.${doMin ? "min." : ""}js`,
-			globalName: libName,
-			minify: doMin,
-			bundle: true,
-			sourcemap: true,
-			platform: "browser",
-			tsconfig: "tsconfig.esbuild.json",
-			format: "iife",
-			incremental: incBuild,
-			logLevel: "info",
-			color: true,
-		})
-		.catch(() => process.exit(1));
+	esb.build({
+		charset: "utf8",
+		entryPoints: [`${srcDir}/index.js`],
+		target: "es5",
+		banner: copyBanner,
+		outfile: `${targDir}/${libName}.${doMin ? "min." : ""}js`,
+		globalName: libName,
+		minify: doMin,
+		bundle: true,
+		sourcemap: true,
+		platform: "browser",
+		tsconfig: "tsconfig.esbuild.json",
+		format: "iife",
+		incremental: incBuild,
+		logLevel: "info",
+		color: true,
+		footer: footer
+	}).catch(() => process.exit(1));
 };
 
 doBuild(minify);
