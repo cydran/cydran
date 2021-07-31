c6222db24c765241ee073478ecc53e7cd76b3c57
Burk Price <webigrator@gmail.com>
2021-07-30 21:43:48 -0600

    linting - method signature formatting fix and spacing

--- src/interface/ables/Watchable.ts
+++ src/interface/ables/Watchable.ts
@@ -1,10 +1,5 @@
 interface Watchable {
-	watch<T>(
-		expression: string,
-		target: (previous: T, current: T) => void,
-		reducerFn?: (input: any) => T,
-		context?: any
-	): void;
+	watch<T>( expression: string, target: (previous: T, current: T) => void, reducerFn?: (input: any) => T, context?: any ): void;
 
 	evaluate<T>(expression: string): T;
 
