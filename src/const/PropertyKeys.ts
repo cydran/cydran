c70d3dd277236d54489b5cc9e6d0d3e005da3380
Burk Price <webigrator@gmail.com>
2021-11-13 05:19:24 -0700

    new property keys

--- src/const/PropertyKeys.ts
+++ src/const/PropertyKeys.ts
@@ -2,8 +2,11 @@ enum PropertyKeys {
 	CYDRAN_DIGEST_MAX_EVALUATIONS = "cydran.digest.maxEvaluations",
 	CYDRAN_CLONE_MAX_EVALUATIONS = "cydran.clone.maxEvaluations",
 	CYDRAN_EQUALS_MAX_EVALUATIONS = "cydran.equals.maxEvaluations",
+	CYDRAN_STARTUP_SYNCHRONOUS = "cydran.startup.synchronous",
 	CYDRAN_PRODUCTION_ENABLED = "cydran.production.enabled",
-	CYDRAN_STARTUP_SYNCHRONOUS = "cydran.startup.synchronous"
+	CYDRAN_PRODUCTION_STARTPHRASE = "cydran.production.startphrase",
+	CYDRAN_LOGGING_LEVEL = "cydran.logging.level",
+	CYDRAN_LOGGING_COLOR_PREFIX = "cydran.logging.color"
 }
 
 export default PropertyKeys;
