760ac3c9e135c2b52c65eb3d1445aab8f166c97c
Burk Price <webigrator@gmail.com>
2021-12-12 00:32:06 -0700

    make sure that the resolver actually has a function

--- src/argument/FunctionArgumentResolver.ts
+++ src/argument/FunctionArgumentResolver.ts
@@ -1,17 +1,21 @@
 import Module from "module/Module";
 import ArgumentResolver from 'argument/ArgumentResolver';
-import { requireNotNull } from 'util/Utils';
+import { requireNotNull, isDefined } from 'util/Utils';
+import { IllegalArgumentError } from "error/Errors";
 
+type WkFunction = ReturnType<() => any>;
 class FunctionArgumentResolver implements ArgumentResolver {
+	private fn: WkFunction;
 
-	private fn: () => any;
-
-	constructor(fn: () => any) {
+	constructor(fn: WkFunction) {
 		this.fn = requireNotNull(fn, "fn");
+		if(typeof fn !== 'function') {
+			throw new IllegalArgumentError("supplied argument not a funciton");
+		}
 	}
 
 	public resolve(module: Module): any {
-		const instance: any = this.fn();
+		const instance: any = (typeof this.fn === 'function') ? this.fn() : null;
 
 		return instance;
 	}
