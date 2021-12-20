81131e5de6467d8a9bba2810711073c0f429704b
Burk Price <webigrator@gmail.com>
2021-12-20 00:55:32 -0700

    fixed the naming

--- test/component/visitor/OtherVisitor.spec.ts
+++ test/component/visitor/OtherVisitor.spec.ts
@@ -1,23 +1,15 @@
 import OtherVisitor from "component/visitor/OtherVisitor";
-import CydranContext from "context/CydranContext";
 import DomImpl from "dom/DomImpl";
 import CydranContextImpl from "context/CydranContextImpl";
 
 let visitor: OtherVisitor = null;
-let cydranContext: CydranContext = null;
-
-function getCydranContext(): CydranContext {
-	return new CydranContext();
-}
 
 beforeEach(() => {
-	cydranContext = new CydranContextImpl(new DomImpl());
-	visitor = new OtherVisitor(cydranContext);
+	visitor = new OtherVisitor(new CydranContextImpl(new DomImpl()));
 });
 
 afterEach(() => {
 	visitor = null;
-	cydranContext = null;
 });
 
 test("instance is good", () => {
