b02cabba7fcb4d1d35433d6bfd75443dcb0e91fb
Matthew Werny <matthewwerny+github@gmail.com>
2021-12-24 23:31:31 -0700

    Enabled integration test

--- integration/bug/bug492.integration-spec.ts
+++ integration/bug/bug492.integration-spec.ts
@@ -1,9 +1,6 @@
-/**
- * @jest-environment jsdom
- */
 import { builder, reset, Stage } from "cydran";
 
-test.skip("set production mode to true", () => {
+test("set production mode to true", () => {
 	reset();
 
 	document.body.innerHTML = '<div id="app"></div>';
@@ -14,13 +11,12 @@ test.skip("set production mode to true", () => {
 	expect(stage.getProperties().isTruthy('cydran.production.enabled')).toEqual(true);
 });
 
-test.skip("expect default mode to be development (production mode == false)", () => {
+test("expect default mode to be development (production mode == false)", () => {
 	reset();
 
 	document.body.innerHTML = '<div id="app"></div>';
 
-	const stage: Stage = builder("#app", {"cydran.logging.level": "WARN"})
-		.build();
+	const stage: Stage = builder("#app", {"cydran.logging.level": "WARN"}).build();
 	stage.start();
 
 	expect(stage.getProperties().isTruthy('cydran.production.enabled')).toEqual(false);
