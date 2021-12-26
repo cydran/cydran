2f3b8dd5a74644e24b6eb01b7240c070e9626e3c
Burk Price <webigrator@gmail.com>
2021-12-26 02:55:40 -0700

    updated coverage of ConsoleOutputStrategy

--- test/logger/ConsoleOutputStrategy.spec.ts
+++ test/logger/ConsoleOutputStrategy.spec.ts
@@ -18,3 +18,28 @@ beforeAll(() => {
 test("ConsoleOutputStrategy is not null", () => {
 	expect(cos).not.toBeNull();
 });
+
+test("log @ DISABLED level", () => {
+	const wkSpy: ConsoleOutputStrategy = jest.spyOn(cos, 'log');
+	cos.log("TEST_CLASS", Level.DISABLED, "should not log", false);
+	expect(wkSpy).toBeCalledTimes(1);
+});
+
+test("log @ INFO level w/ FULLSTACK", () => {
+	const wkSpy: ConsoleOutputStrategy = jest.spyOn(cos, 'log');
+	cos.log("TEST_CLASS", Level.INFO, "with full stack", true);
+	expect(wkSpy).toBeCalledTimes(1);
+});
+
+test("log @ every level", () => {
+	const wkSpy: ConsoleOutputStrategy = jest.spyOn(cos, 'log');
+	Object.keys(Level)
+		.filter(k => !/\d+/.test(k))
+		.forEach(key => {
+			const wkLvl: Level = Level[key];
+			cos.log("TEST_CLASS", wkLvl, `level logged = ${ key }`, false);
+			expect(wkSpy).toBeCalledTimes(1);
+			wkSpy.mockClear();
+		}
+	);
+});
