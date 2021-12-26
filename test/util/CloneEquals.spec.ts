c94ef036b32aa2f456e99dc09807b1bc834f9c60
Burk Price <webigrator@gmail.com>
2021-12-26 03:30:00 -0700

    skeleton for unit tests

---
+++ test/util/CloneEquals.spec.ts
@@ -0,0 +1,9 @@
+import { cloneDeep, isEqual } from "util/CloneEquals";
+
+test.skip("cloneDeep", () => {
+	// TODO: flesh out the unit
+});
+
+test.skip("isEquals", () => {
+	// TODO: flesh out the unit
+});
