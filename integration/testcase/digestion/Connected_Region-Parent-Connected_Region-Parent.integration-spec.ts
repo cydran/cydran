61288519dad27f1254469f8d18ac230ec2e7c260
Matthew Werny <matthewwerny+github@gmail.com>
2021-12-26 17:22:30 -0700

    Added integration test

--- integration/testcase/digestion/Connected_Region-Parent-Connected_Region-Parent.integration-spec.ts
+++ integration/testcase/digestion/Connected_Region-Parent-Connected_Region-Parent.integration-spec.ts
@@ -1,5 +1,122 @@
-test.skip("TODO - Implement", () => {
+import { Component } from 'cydran';
+import Harness from '../../Harness';
+import LoggingSegmentDigester from "./LoggingSegmentDigester";
 
-	// TODO - Implement
+const GRAND_PARENT_TEMPLATE: string = `<div>
+	<script type="cydran/region" c:region:name="child" c:region:value="m().values"></script>
+	<p data-testid="grand-parent">{{m().values.value}}</p>
+</div>`;
 
+const PARENT_TEMPLATE: string = `<div>
+	<script type="cydran/region" c:region:name="child" c:region:value="v()"></script>
+	<p data-testid="parent">{{v().value}}</p>
+</div>`;
+
+const CHILD_TEMPLATE: string = `<div>
+	<p data-testid="child">{{v().value}}</p>
+	<button c:onclick="m().update()">Change Value</button>
+</div>`;
+
+interface ValueContainer {
+
+	value: string;
+
+}
+
+class GrandParentComponent extends Component {
+
+	private values: ValueContainer;
+
+	constructor() {
+		super(GRAND_PARENT_TEMPLATE);
+		this.values = {
+			value: "Alpha"
+		};
+	}
+
+}
+
+class ParentComponent extends Component {
+
+	constructor() {
+		super(PARENT_TEMPLATE);
+	}
+
+}
+
+class ChildComponent extends Component {
+
+	constructor() {
+		super(CHILD_TEMPLATE);
+	}
+
+	public update(): void {
+		this.getValue().value = "Beta";
+	}
+
+}
+
+test("Connected Region -> Parent -> Connected Region -> Parent", () => {
+	const segmentDigester: LoggingSegmentDigester = new LoggingSegmentDigester();
+
+	const harness: Harness<GrandParentComponent> = new Harness<GrandParentComponent>(() => new GrandParentComponent(), {
+		"cydran.internal.factory.segment-digester": () => segmentDigester
+	});
+
+	harness.start();
+
+	const parent: ParentComponent = new ParentComponent();
+	const child: ChildComponent = new ChildComponent();
+
+	parent.setChild("child", child);
+
+	harness.getComponent().setChild("child", parent);
+
+	harness.forTestId("grand-parent")
+		.expect()
+		.textContent()
+		.toEqual("Alpha");
+
+	harness.forTestId("parent")
+		.expect()
+		.textContent()
+		.toEqual("Alpha");
+
+	harness.forTestId("child")
+		.expect()
+		.textContent()
+		.toEqual("Alpha");
+
+	harness.forText("Change Value")
+		.get()
+		.click();
+
+	harness.forTestId("grand-parent")
+		.expect()
+		.textContent()
+		.toEqual("Beta");
+
+	harness.forTestId("parent")
+		.expect()
+		.textContent()
+		.toEqual("Beta");
+
+	harness.forTestId("child")
+		.expect()
+		.textContent()
+		.toEqual("Beta");
+
+	expect(segmentDigester.getEvents()).toEqual([
+		'0-0-8 - Evaluating - v().value',
+		'0-0-8 - Changed - v().value',
+		'0-0-8 - Evaluating - m().update()',
+		'0-0-5 - Evaluating - v().value',
+		'0-0-5 - Changed - v().value',
+		'0-0-2 - Evaluating - m().values.value',
+		'0-0-2 - Changed - m().values.value',
+		'0-0-8 - Evaluating - v().value',
+		'0-0-8 - Evaluating - m().update()',
+		'0-0-5 - Evaluating - v().value',
+		'0-0-2 - Evaluating - m().values.value'
+	]);
 });
