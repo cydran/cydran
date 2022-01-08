36c92621dcfea44a20b77ab2631ae154fe33b84a
Burk Price <webigrator@gmail.com>
2022-01-07 17:37:15 -0700

    Updated unit tests to use "-" delimiter

--- test/component/AttributesImpl.spec.ts
+++ test/component/AttributesImpl.spec.ts
@@ -22,69 +22,69 @@ test("AttributesImpl constructor - null prefix", () => {
 test("AttributesImpl constructor - invalid prefix character at start", () => {
 	let thrown = null;
 	try {
-		new AttributesImpl(":foo");
+		new AttributesImpl("-foo");
 	} catch (e) {
 		thrown = e;
 	}
 
 	expect(thrown).not.toBeNull();
-	expect(thrown.message).toEqual("Malformed prefix: :foo");
+	expect(thrown.message).toEqual("Malformed prefix: -foo");
 });
 
 test("AttributesImpl constructor - invalid prefix character at end", () => {
 	let thrown = null;
 	try {
-		new AttributesImpl("foo:");
+		new AttributesImpl("foo-");
 	} catch (e) {
 		thrown = e;
 	}
 
 	expect(thrown).not.toBeNull();
-	expect(thrown.message).toEqual("Malformed prefix: foo:");
+	expect(thrown.message).toEqual("Malformed prefix: foo-");
 });
 
 test("AttributesImpl extract()", () => {
 	const element: HTMLElement = document.createElement("div");
-	element.setAttribute("c:first", "one");
+	element.setAttribute("c-first", "one");
 
-	expect(element.hasAttribute("c:first")).toBeTruthy();
-	expect(element.getAttribute("c:first")).toEqual("one");
+	expect(element.hasAttribute("c-first")).toBeTruthy();
+	expect(element.getAttribute("c-first")).toEqual("one");
 
 	expect(SPECIMEN.extract(element, "first")).toEqual("one");
 });
 
 test("AttributesImpl remove()", () => {
 	const element: HTMLElement = document.createElement("div");
-	element.setAttribute("c:first", "one");
-	element.setAttribute("c:second", "two");
-	element.setAttribute("c:third", "three");
+	element.setAttribute("c-first", "one");
+	element.setAttribute("c-second", "two");
+	element.setAttribute("c-third", "three");
 
-	expect(element.hasAttribute("c:first")).toBeTruthy();
-	expect(element.hasAttribute("c:second")).toBeTruthy();
-	expect(element.hasAttribute("c:third")).toBeTruthy();
-	expect(element.getAttribute("c:first")).toEqual("one");
-	expect(element.getAttribute("c:second")).toEqual("two");
-	expect(element.getAttribute("c:third")).toEqual("three");
+	expect(element.hasAttribute("c-first")).toBeTruthy();
+	expect(element.hasAttribute("c-second")).toBeTruthy();
+	expect(element.hasAttribute("c-third")).toBeTruthy();
+	expect(element.getAttribute("c-first")).toEqual("one");
+	expect(element.getAttribute("c-second")).toEqual("two");
+	expect(element.getAttribute("c-third")).toEqual("three");
 
 	SPECIMEN.remove(element, "second");
 
-	expect(element.hasAttribute("c:first")).toBeTruthy();
-	expect(element.hasAttribute("c:second")).toBeFalsy();
-	expect(element.hasAttribute("c:third")).toBeTruthy();
-	expect(element.getAttribute("c:first")).toEqual("one");
-	expect(element.getAttribute("c:third")).toEqual("three");
+	expect(element.hasAttribute("c-first")).toBeTruthy();
+	expect(element.hasAttribute("c-second")).toBeFalsy();
+	expect(element.hasAttribute("c-third")).toBeTruthy();
+	expect(element.getAttribute("c-first")).toEqual("one");
+	expect(element.getAttribute("c-third")).toEqual("three");
 });
 
 test("AttributesImpl isEventAttribute() - with event attribute", () => {
-	expect(SPECIMEN.isEventAttribute("c:onclick")).toBeTruthy();
+	expect(SPECIMEN.isEventAttribute("c-onclick")).toBeTruthy();
 });
 
 test("AttributesImpl isEventAttribute() - with malformed event Cydran attribute", () => {
-	expect(SPECIMEN.isEventAttribute("c:onbogus:event")).toBeFalsy();
+	expect(SPECIMEN.isEventAttribute("c-onbogus-event")).toBeFalsy();
 });
 
 test("AttributesImpl isEventAttribute() - with non-event Cydran attribute", () => {
-	expect(SPECIMEN.isEventAttribute("c:foo")).toBeFalsy();
+	expect(SPECIMEN.isEventAttribute("c-foo")).toBeFalsy();
 });
 
 test("AttributesImpl isEventAttribute() - with non-event Cydran attribute", () => {
@@ -94,11 +94,11 @@ test("AttributesImpl isEventAttribute() - with non-event Cydran attribute", () =
 // TODO - Exclude behavior params attributes from truthiness
 
 test("AttributesImpl isBehaviorAttribute() - valid behavior attribute", () => {
-	expect(SPECIMEN.isBehaviorAttribute("c:foo")).toBeTruthy();
+	expect(SPECIMEN.isBehaviorAttribute("c-foo")).toBeTruthy();
 });
 
 test("AttributesImpl isBehaviorAttribute() - behavior param attribute", () => {
-	expect(SPECIMEN.isBehaviorAttribute("c:foo:bar")).toBeFalsy();
+	expect(SPECIMEN.isBehaviorAttribute("c-foo-bar")).toBeFalsy();
 });
 
 test("AttributesImpl isBehaviorAttribute() - non-behavior attribute", () => {
@@ -106,15 +106,15 @@ test("AttributesImpl isBehaviorAttribute() - non-behavior attribute", () => {
 });
 
 test("AttributesImpl extractEventName()", () => {
-	expect(SPECIMEN.extractEventName("c:onclick")).toEqual("click");
+	expect(SPECIMEN.extractEventName("c-onclick")).toEqual("click");
 });
 
 test("AttributesImpl extractBehaviorName()", () => {
-	expect(SPECIMEN.extractBehaviorName("c:bar")).toEqual("bar");
+	expect(SPECIMEN.extractBehaviorName("c-bar")).toEqual("bar");
 });
 
 test("AttributesImpl asTypePrefix()", () => {
-	expect(SPECIMEN.asTypePrefix("bar")).toEqual("c:bar");
+	expect(SPECIMEN.asTypePrefix("bar")).toEqual("c-bar");
 });
 
 test("AttributesImpl getPrefix()", () => {
