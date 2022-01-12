790b2f637e1aa66008fcbea97adb53de64d27d58
Burk Price <webigrator@gmail.com>
2022-01-12 00:02:32 -0700

    added updateLogLevel
    * also provided updated tsdoc for extant method and for the new

--- src/log/LoggerFactory.ts
+++ src/log/LoggerFactory.ts
@@ -5,11 +5,21 @@ import { Properties } from "properties/Property";
 
 class LoggerFactory {
 	/**
-	 * Get the named logger
+	 * Get the named {@link Logger | logger}
 	 * @param name of the associated logger
+	 * @returns a Logger reference
 	 */
 	public static getLogger(name: string, props?: Properties): Logger {
 		return new LoggerImpl(name, LoggerServiceImpl.INSTANCE(props));
+	}
+
+	/**
+	 * Set change/modify the log {@link Level | level} during runtime. Any string not matching a valid level will cause an error message to be generated as long as logging isn't currently {@link Level#DISABLED | disabled}
+	 * @param level non-casesensitive string representation of {@link Level | a qualified level}.
+	 * @returns void
+	 */
+	public static updateLogLevel(level: string): void {
+		LoggerServiceImpl.INSTANCE().setLevelByName(level);
 	}
 }
 
