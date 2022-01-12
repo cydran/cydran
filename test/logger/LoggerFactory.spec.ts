8c17824d8a4f2bb39655da1054b83a47be9b3a57
Burk Price <1290008+evilchia@users.noreply.github.com>
2022-01-12 15:17:45 -0700

    Feature/allow runtime log level adjustment resolved #409 (#556)
    * updated tslint max-line-length
    
    ignore the rule when specific patterns are found - primarily tsdoc, comments, imports and class declarations
    
    * added updateLogLevel
    
    * also provided updated tsdoc for extant method and for the new
    
    * get the current set logging level
    
    * updated tsdocs
    
    * new method for getting current logging level
    
    used internally as well as by LoggerFactory
    
    * udpated docs
    
    * Don't do anything if the level is already set at the same level
    
    * updated method signature and docs
    
    * new units for new methods

--- test/logger/LoggerFactory.spec.ts
+++ test/logger/LoggerFactory.spec.ts
@@ -4,3 +4,32 @@ import { assertNullGuarded } from "test/TestUtils";
 test("getLogger() - null name", () => {
 	assertNullGuarded("name", () => LoggerFactory.getLogger(null));
 });
+
+test("getLevelAsString()", () => {
+	const result: string = LoggerFactory.currentLevel();
+	expect(result).toEqual("INFO");
+});
+
+test("updateLevel(name: string)", () => {
+	expect(LoggerFactory.currentLevel()).toEqual("INFO");
+	const enumStates: string[] = ["TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL", "DISABLED"].reverse();
+	const results: {}[] = [];
+	enumStates.forEach((val) => {
+		try {
+			LoggerFactory.updateLevel(val);
+			const current: string = LoggerFactory.currentLevel();
+			results.push({'in': val, 'result': current});
+			expect(current).toEqual(val);
+		} catch (err) {
+			fail(`bad value passed: ${ val }`);
+		}
+	});
+	console.table(results);
+
+	const baseline: string = "TRACE";
+	LoggerFactory.updateLevel(baseline);
+	expect(LoggerFactory.currentLevel()).toEqual(baseline);
+	const badVal: string = "whacko";
+	LoggerFactory.updateLevel(badVal);
+	expect(LoggerFactory.currentLevel()).toEqual(baseline);
+});
