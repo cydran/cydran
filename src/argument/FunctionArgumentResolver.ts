8fd84d7fc9aa510d072f52b27c4b7d675fe865a0
Burk Price <webigrator@gmail.com>
2022-01-16 01:59:16 -0700

    last of JSType cleanup

--- src/argument/FunctionArgumentResolver.ts
+++ src/argument/FunctionArgumentResolver.ts
@@ -2,6 +2,7 @@ import Module from "module/Module";
 import ArgumentResolver from 'argument/ArgumentResolver';
 import { requireNotNull, isDefined } from 'util/Utils';
 import { IllegalArgumentError } from "error/Errors";
+import JSType from "const/JSType";
 
 type WkFunction = ReturnType<() => any>;
 class FunctionArgumentResolver implements ArgumentResolver {
@@ -9,13 +10,13 @@ class FunctionArgumentResolver implements ArgumentResolver {
 
 	constructor(fn: WkFunction) {
 		this.fn = requireNotNull(fn, "fn");
-		if(typeof fn !== 'function') {
+		if(typeof fn !== JSType.FN) {
 			throw new IllegalArgumentError("supplied argument not a funciton");
 		}
 	}
 
 	public resolve(module: Module): any {
-		const instance: any = (typeof this.fn === 'function') ? this.fn() : null;
+		const instance: any = (typeof this.fn === JSType.FN) ? this.fn() : null;
 
 		return instance;
 	}
