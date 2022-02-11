45d58dc7fed6f06e4ca005873bb3b8fa83a69e7c
Burk Price <webigrator@gmail.com>
2022-02-11 16:59:26 -0700

    export the padText Fn

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import Component from "component/Component";
 import Filters from "filter/Filters";
 import { builder, argumentsBuilder } from "const/Builder";
 import create from "config/Create";
-import { isDefined, requireNotNull, requireValid, setStrictTypeChecksEnabled, merge, overlay } from "util/Utils";
+import { isDefined, requireNotNull, requireValid, setStrictTypeChecksEnabled, merge, overlay, padText } from "util/Utils";
 import Events from "const/EventsFields";
 import { CYDRAN_KEY, Ids } from "Constants";
 import JSType from "const/JSType";
@@ -73,6 +73,7 @@ export {
 	noConflict,
 	requireNotNull,
 	requireValid,
+	padText,
 	setStrictTypeChecksEnabled,
 	Ids,
 	JSType,
