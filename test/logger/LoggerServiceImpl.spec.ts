054ee0756c7b23ad7525ea3a7e06b6590e7de4e4
Burk Price <webigrator@gmail.com>
2022-02-11 22:34:31 -0700

    fix linting issue

--- test/logger/LoggerServiceImpl.spec.ts
+++ test/logger/LoggerServiceImpl.spec.ts
@@ -22,6 +22,7 @@ test("setLevelByName - bad value", () => {
 
 test("LoggerService setLevel", () => {
 	enumKeys(Level).forEach(k => {
+		// tslint:disable-next-line
 		console.log(`Setting level: ${ k }`);
 		const wkLvl: Level = Level[k];
 		ls.setLevel(wkLvl);
