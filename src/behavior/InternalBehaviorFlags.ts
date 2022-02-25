7e41fccabdc145a40f6c238798ccb228cb8e54ac
Matthew Werny <matthewwerny+github@gmail.com>
2022-02-25 00:33:00 -0700

    Broke behavior flags out for the internal ones

---
+++ src/behavior/InternalBehaviorFlags.ts
@@ -0,0 +1,6 @@
+enum InternalBehaviorFlags {
+	PROPAGATION = "PROPAGATION",
+	EXPRESSION_DISALLOWED = "EXPRESSION_DISALLOWED"
+}
+
+export default InternalBehaviorFlags;
--- test/behavior/BehaviorFlags.spec.ts
+++ test/behavior/BehaviorFlags.spec.ts
@@ -1,7 +1,7 @@
 import BehaviorFlags from "behavior/BehaviorFlags";
 import { isDefined } from "util/Utils";
 
-const enumKeys: string[] = "ROOT_PROHIBITED,PROPAGATION,CHILD_CONSUMPTION_PROHIBITED".split(",");
+const enumKeys: string[] = "ROOT_PROHIBITED,CHILD_CONSUMPTION_PROHIBITED".split(",");
 
 test("all accounted for", () => {
 	for(const key of enumKeys) {
@@ -14,7 +14,6 @@ test("associated values are correct", () => {
 	for(const key of enumKeys) {
 		switch(key) {
 			case BehaviorFlags.ROOT_PROHIBITED:
-			case BehaviorFlags.PROPAGATION:
 			case BehaviorFlags.CHILD_CONSUMPTION_PROHIBITED:
 				expect(isDefined(BehaviorFlags[key.toUpperCase()])).toBe(true);
 				break;
