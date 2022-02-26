beb77eb3c508b8667d5efeaca625ee610a571de5
Burk Price <webigrator@gmail.com>
2022-02-26 06:59:14 -0700

    Using OutputStrategyProvider

--- src/log/LoggerFactory.ts
+++ src/log/LoggerFactory.ts
@@ -3,13 +3,12 @@ import LoggerImpl from "log/LoggerImpl";
 import LoggerServiceImpl from "log/LoggerServiceImpl";
 import { Properties } from "properties/Property";
 import { isDefined, requireNotNull } from "util/Utils";
-import PropertiesImpl from "properties/PropertiesImpl";
 import Level from "log/Level";
-import { IllegalArgumentError } from "error/Errors";
 import OutputStrategy from "log/OutputStrategy";
 
 class LoggerFactory {
 	private static loggerSvc: LoggerServiceImpl;
+	private static wkLog: Logger;
 
 	/**
 	 * Get the named {@link Logger | logger}
@@ -18,8 +17,7 @@ class LoggerFactory {
 	 * @returns a Logger reference
 	 */
 	public static getLogger(name: string, level?: string, strategy: OutputStrategy = null): Logger {
-		this.guardService();
-		const retLogger: Logger = new LoggerImpl(requireNotNull(name, "name"), this.loggerSvc, strategy);
+		const retLogger: Logger = new LoggerImpl(requireNotNull(name, "name"), this.guardService(), strategy);
 
 		if (isDefined(level)) {
 			try {
@@ -32,6 +30,26 @@ class LoggerFactory {
 		return retLogger;
 	}
 
+	public static registerOutputStrategy(key: string, strat: OutputStrategy): void {
+		try {
+			this.guardService().registerOutputStrategy(key, strat);
+		} catch (err) {
+			this.wkLog.ifError(() => err.message, err);
+		}
+	}
+
+	public static removeOutputStrategy(key: string): void {
+		try {
+			this.guardService().removeOutputStrategy(key);
+		} catch (err) {
+			this.wkLog.ifError(() => err.message, err);
+		}
+	}
+
+	public static setPrefsForStrategy(key: string, props: Properties): void {
+		this.guardService().setPrefsForStrategy(key, props);
+	}
+
 	/**
 	 * Set change/modify the log level during runtime.
 	 * Any string not matching a valid level will cause an error message to be generated as long as logging isn't currently set above error
@@ -40,12 +58,7 @@ class LoggerFactory {
 	 * @returns void
 	 */
 	public static updateLevel(level: string): void {
-		this.guardService();
-		try {
-			this.loggerSvc.setLevelByName(level);
-		} catch (err) {
-			// noop();
-		}
+		this.guardService().setLevelByName(level);
 	}
 
 	/**
@@ -53,8 +66,7 @@ class LoggerFactory {
 	 * @returns string representation of the current logging level
 	 */
 	public static currentLevel(): string {
-		this.guardService();
-		return this.loggerSvc.getLevelAsString();
+		return this.guardService().getLevelAsString();
 	}
 
 	/**
@@ -62,24 +74,23 @@ class LoggerFactory {
 	 * @param props
 	 */
 	public static setPreferences(props: Properties): void {
-		this.guardService();
-		this.loggerSvc.setPreferences(props);
+		this.guardService().updateServicePrefs(props);
 	}
 
-	private static getLevelByName(name: string = "null"): Level | never {
+	private static getLevelByName(name: string = "null"): Level {
 		const newLevel: Level = Level[name.toUpperCase()];
-		if (isDefined(newLevel)) {
-			return newLevel;
-		} else {
-			throw new IllegalArgumentError(`"${ name.toUpperCase() }" not a valid logging level`);
+		if (!isDefined(newLevel)) {
+			this.wkLog.ifDebug(() => `"${ name.toUpperCase() }" not a valid logging level`);
 		}
+		return newLevel;
 	}
 
-
-	private static guardService(props: any = new PropertiesImpl()): void {
+	private static guardService(props?: Properties): LoggerServiceImpl {
 		if (!isDefined(this.loggerSvc)) {
 			this.loggerSvc = new LoggerServiceImpl(props);
+			this.wkLog = new LoggerImpl("LoggerFactory", this.loggerSvc);
 		}
+		return this.loggerSvc;
 	}
 }
 
