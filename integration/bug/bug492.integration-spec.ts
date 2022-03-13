09f3328fd8cd89a23eb93091e4f96ce6f82addd0
Burk Price <webigrator@gmail.com>
2022-03-13 03:44:10 -0600

    concept change
    * from production to strict
    * from development to lazy

--- integration/bug/bug492.integration-spec.ts
+++ integration/bug/bug492.integration-spec.ts
@@ -5,10 +5,10 @@ test("set production mode to true", () => {
 
 	document.body.innerHTML = '<div id="app"></div>';
 
-	const stage: Stage = builder("#app", { "cydran.production.enabled": true, "cydran.logging.level": "WARN" }).build();
+	const stage: Stage = builder("#app", { "cydran.strict.enabled": true, "cydran.logging.level": "WARN" }).build();
 	stage.start();
 
-	expect(stage.getProperties().isTruthy('cydran.production.enabled')).toEqual(true);
+	expect(stage.getProperties().isTruthy('cydran.strict.enabled')).toEqual(true);
 });
 
 test("expect default mode to be development (production mode == false)", () => {
@@ -19,5 +19,5 @@ test("expect default mode to be development (production mode == false)", () => {
 	const stage: Stage = builder("#app", {"cydran.logging.level": "WARN"}).build();
 	stage.start();
 
-	expect(stage.getProperties().isTruthy('cydran.production.enabled')).toEqual(false);
+	expect(stage.getProperties().isTruthy('cydran.strict.enabled')).toEqual(false);
 });
