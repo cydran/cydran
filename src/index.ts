675867bf7cee425a5f672bf79e17425ea04f2395
Burk Price <webigrator@gmail.com>
2022-05-07 16:30:17 -0600

    no need for i18nContext

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,6 @@ import StageBuilder from "stage/StageBuilder";
 import { Bundle, BundleContext, BundleGroup, BundleCategory, BundleItem } from "i18n/Bundle";
 import AbstractBundleResolver from "i18n/AbstractBundleResolver";
 import Nameable from "interface/ables/Nameable";
-import I18nResolvable from "interface/ables/I18nResolvable";
 
 const ORIGINAL_CYDRAN: any = window[CYDRAN_KEY];
 
@@ -76,7 +75,6 @@ export {
 	Bundle, BundleContext, BundleGroup, BundleCategory, BundleItem,
 	Dom,
 	Nameable,
-	I18nResolvable,
 	Component,
 	Nestable,
 	ElementOperations,
