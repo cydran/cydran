0ae16edd4d45c302c376ab395eb545ee04cf2ce3
Matthew Werny <matthewwerny+github@gmail.com>
2022-05-07 15:32:20 -0600

    Checkpoint commit

--- src/behavior/core/EachBehavior.ts
+++ src/behavior/core/EachBehavior.ts
@@ -110,6 +110,10 @@ class EachBehavior extends AbstractContainerBehavior<any[], HTMLElement, EachAtt
 		}
 	}
 
+	protected initializeChildElements(items: any[]): void {
+
+	}
+
 	private initScope(): void {
 		const localScope = new ScopeImpl();
 		const modelFn: () => any = () => this.getModelFn();
