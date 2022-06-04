722305d1e095980047a0f80fd0d0dd7ba375130a
Burk Price <webigrator@gmail.com>
2022-06-03 23:22:23 -0600

    new implementation of an http resource resolver

---
+++ src/http/SimpleHttpClientImpl.ts
@@ -0,0 +1,31 @@
+import { HttpClientError } from "error/Errors";
+import ResourceResolver from "interface/ResourceResolver";
+
+class SimpleHttpClientImpl implements ResourceResolver {
+
+	constructor() {
+		// nothing for now
+	}
+
+	public resource(url: string, callback: Function, method: string = "GET"): any {
+		const xhr: XMLHttpRequest = new XMLHttpRequest();
+		xhr.open(method, url);
+
+		xhr.onreadystatechange = () => {
+			if( xhr.readyState === XMLHttpRequest.DONE) {
+				const status = xhr.status;
+				if(status === 0 || status >= 200 && status < 400) {
+					callback(xhr.response);
+				} else {
+					throw new HttpClientError(`${xhr.status} - ${xhr.statusText}`);
+				}
+			}
+		};
+		xhr.onerror = (err) => {
+			throw new HttpClientError(err["message"]);
+		};
+		xhr.send();
+	}
+}
+
+export default SimpleHttpClientImpl;
