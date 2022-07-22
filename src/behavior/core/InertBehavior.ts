177d7c4ca35bc8214338de047e5b5bc00d99e328
Matthew Werny <matthewwerny+github@gmail.com>
2022-07-21 21:29:15 -0600

    Added inert behavior (#628)

---
+++ src/behavior/core/InertBehavior.ts
@@ -0,0 +1,18 @@
+import { asBoolean } from "util/AsFunctions";
+import AbstractValueBehavior from "behavior/AbstractValueBehavior";
+
+class InertBehavior extends AbstractValueBehavior<boolean, HTMLInputElement, any> {
+
+	constructor() {
+		super();
+		this.setReducerFn(asBoolean);
+	}
+
+	protected onChange(previous: boolean, current: boolean): void {
+		// Keyed access to field due to newness of inert in HTMLElement spec such that it hasn't made into TypeScript yet
+		this.getEl()["inert"] = current;
+	}
+
+}
+
+export default InertBehavior;
