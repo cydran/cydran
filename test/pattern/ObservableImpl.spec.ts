d6db27c01231c148070b77eb5cb05f1655d43367
Matthew Werny <matthewwerny+github@gmail.com>
2023-04-05 16:48:20 -0600

    Checkpoint commit

--- test/pattern/ObservableImpl.spec.ts
+++ test/pattern/ObservableImpl.spec.ts
@@ -1,56 +1,48 @@
 import Observable from "pattern/Observable";
 import ObservableImpl from "pattern/ObservableImpl";
 
-test("Garbage Collection does not retain references with single argument", async () => {
-	const specimen: Observable<string> = new ObservableImpl<string>();
-	const results: string[] = [];
+// NOTE - These two test cases (single and multiple) are stuffed into the same test due to triggering
+// garbage collection in two different tests seems to consistently and spectacularly fail.
 
-	let callback: (value: string) => void = (value: string) => results.push(value);
+test("Garbage Collection does not retain references with single and multiple arguments", async () => {
+	const singleSpecimen: Observable<string> = new ObservableImpl<string>();
+	const multipleSpecimen: Observable<string> = new ObservableImpl<string>();
+	const singleResults: string[] = [];
+	const multipleFirstResults: string[] = [];
+	const multipleSecondResults: string[] = [];
 
-	specimen.register(callback);
-	specimen.notify("foo");
-	specimen.notify("bar");
-
-	callback = null as unknown as (value: string) => void;
-
-	await triggerGarbageCollection();
-
-	specimen.notify("bat");
-	specimen.notify("baz");
-
-	expect(results.length).toEqual(2);
-	expect(results[0]).toEqual("foo");
-	expect(results[1]).toEqual("bar");
-}, 30000);
-
-test("Garbage Collection does not retain references with multiple arguments", async () => {
-	const specimen: Observable<string> = new ObservableImpl<string>();
-	const firstResults: string[] = [];
-	const secondResults: string[] = [];
-
-	let callback: (first: string, second: string) => void = (first: string, second: string) => {
-		firstResults.push(first);
-		secondResults.push(second);
+	let singleCallback: (value: string) => void = (value: string) => singleResults.push(value);
+	let multipleCallback: (first: string, second: string) => void = (first: string, second: string) => {
+		multipleFirstResults.push(first);
+		multipleSecondResults.push(second);
 	};
 
-	specimen.register(callback);
-	specimen.notify("foo", "Alpha");
-	specimen.notify("bar", "Beta");
+	singleSpecimen.register(singleCallback);
+	singleSpecimen.notify("foo");
+	singleSpecimen.notify("bar");
+	multipleSpecimen.register(multipleCallback);
+	multipleSpecimen.notify("foo", "Alpha");
+	multipleSpecimen.notify("bar", "Beta");
 
-	callback = null as unknown as (value: string) => void;
+	singleCallback = null as unknown as (value: string) => void;
+	multipleCallback = null as unknown as (value: string) => void;
 
 	await triggerGarbageCollection();
 
-	specimen.notify("bat", "Gamma");
-	specimen.notify("baz", "Delta");
+	singleSpecimen.notify("bat");
+	singleSpecimen.notify("baz");
+	multipleSpecimen.notify("bat", "Gamma");
+	multipleSpecimen.notify("baz", "Delta");
 
-	expect(firstResults.length).toEqual(2);
-	expect(firstResults[0]).toEqual("foo");
-	expect(firstResults[1]).toEqual("bar");
-
-	expect(secondResults.length).toEqual(2);
-	expect(secondResults[0]).toEqual("Alpha");
-	expect(secondResults[1]).toEqual("Beta");
+	expect(singleResults.length).toEqual(2);
+	expect(singleResults[0]).toEqual("foo");
+	expect(singleResults[1]).toEqual("bar");
+	expect(multipleFirstResults.length).toEqual(2);
+	expect(multipleFirstResults[0]).toEqual("foo");
+	expect(multipleFirstResults[1]).toEqual("bar");
+	expect(multipleSecondResults.length).toEqual(2);
+	expect(multipleSecondResults[0]).toEqual("Alpha");
+	expect(multipleSecondResults[1]).toEqual("Beta");
 }, 30000);
 
 async function triggerGarbageCollection() {
