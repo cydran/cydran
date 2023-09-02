c5dc388f905dfddeee4a5ad60eb1bd86245a8334
Burk Price <webigrator@gmail.com>
2023-09-02 03:16:03 -0600

    new units

---
+++ test/component/ComponentTransitions.spec.ts
@@ -0,0 +1,25 @@
+import ComponentTransitions from "component/ComponentTransitions";
+
+const spec: Object = ComponentTransitions;
+const oName: string = "ComponentTransitions";
+
+describe(`Expected constants of ${ oName }`, () => {
+	const enumStates: string[] = Object.keys(spec).map(k => { return spec[k]; });
+
+	test.each(enumStates)(`${ oName } availability of state for: '%s'`, (val) => {
+		try {
+			const wkVal: string = val.toUpperCase();
+			const wkObj: any = spec[wkVal];
+			expect(wkObj).not.toBeNull();
+			expect(wkObj).toEqual(val);
+		} catch (err) {
+			throw err;
+		}
+	});
+
+	const badVal: string = "Whack";
+	test(`failure with bad value: '${badVal}'`, () => {
+		expect(spec[badVal]).toBeUndefined();
+	});
+
+});
