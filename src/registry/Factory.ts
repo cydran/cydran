8eb5b156efe4de12bc3149b67b93c2290412692e
Matthew Werny <matthewwerny+github@gmail.com>
2023-09-06 23:04:35 -0600

    Added TODO comment

--- src/registry/Factory.ts
+++ src/registry/Factory.ts
@@ -3,6 +3,8 @@ import Disposable from "interface/ables/Disposable";
 
 interface Factory<T> extends Disposable {
 
+	// TODO - Support passing arguments for argument argument resolver
+
 	get(gettable: Gettable): T;
 
 }
