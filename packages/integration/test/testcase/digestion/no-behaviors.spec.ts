b960ba3b5e28577db46e15bd7e53decb4cfe67e6
Matthew Werny <matthewwerny+github@gmail.com>
2024-04-04 23:10:30 -0600

    Migrated integration tests to dedicated package

---
+++ packages/integration/test/testcase/digestion/no-behaviors.spec.ts
@@ -0,0 +1,26 @@
+import { Component } from "@cydran/cydran";
+import { Harness } from "@cydran/testsupport";
+import LoggingSegmentDigester from "./LoggingSegmentDigester";
+
+class TestComponent extends Component {
+
+	constructor() {
+		super("<div>Hello World!</div>");
+	}
+
+}
+
+test.skip("Digestion - No behaviors", () => {
+	document.body.innerHTML = '<div></div>';
+
+	const segmentDigester: LoggingSegmentDigester = new LoggingSegmentDigester();
+
+	const harness: Harness<TestComponent> = new Harness<TestComponent>(() => new TestComponent(), {
+		"cydran.internal.factory.segment-digester": () => segmentDigester
+	});
+
+	harness.start();
+
+	expect(harness.getDocument().body.innerHTML).toEqual("<div>Hello World!</div>");
+	expect(segmentDigester.getEvents().length).toEqual(0);
+});
