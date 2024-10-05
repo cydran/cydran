b415100f21071336115aa0bbf99ab8776f3a011f
Burk Price <evilchia@gmail.com>
2024-10-05 11:18:20 -0600

    better organized

---
+++ packages/cydran/test/logger/appender/ConsoleOutputStrategy.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect, beforeAll, jest } from "@jest/globals";
+import Level from 'log/Level';
+import ConsoleOutputStrategy from 'log/appender/ConsoleOutputStrategy';
+import PropertiesImpl from "properties/PropertiesImpl";
+import { PropertyKeys } from "CydranConstants";
+
+const msg: string = "test payload";
+let cos: ConsoleOutputStrategy = null;
+
+beforeAll(() => {
+	const p: {} = {
+		[PropertyKeys.CYDRAN_LOG_LEVEL]: "trace"
+	};
+	const wkprops: PropertiesImpl = new PropertiesImpl();
+	wkprops.load(p);
+	cos = new ConsoleOutputStrategy(wkprops);
+});
+
+test("ConsoleOutputStrategy is not null", () => {
+	expect(cos).not.toBeNull();
+});
+
+test("log @ DISABLED level", () => {
+	const wkSpy: ConsoleOutputStrategy = jest.spyOn(cos, 'log');
+	cos.log("TEST_CLASS", Level.DISABLED, "should not log", false);
+	expect(wkSpy).toBeCalledTimes(1);
+});
+
+test("log @ INFO level w/ FULLSTACK", () => {
+	const wkSpy: ConsoleOutputStrategy = jest.spyOn(cos, 'log');
+	cos.log("TEST_CLASS", Level.INFO, "with full stack", true);
+	expect(wkSpy).toBeCalledTimes(1);
+});
+
+test("log @ every level", () => {
+	const wkSpy: ConsoleOutputStrategy = jest.spyOn(cos, 'log');
+	Object.keys(Level)
+		.filter(k => !/\d+/.test(k))
+		.forEach(key => {
+			const wkLvl: Level = Level[key];
+			cos.log("TEST_CLASS", wkLvl, `level logged = ${ key }`, false);
+			expect(wkSpy).toBeCalledTimes(1);
+			wkSpy.mockClear();
+		}
+	);
+});
