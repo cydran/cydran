8f96dd1582f8bdc56fd085ffdefd62cbdccf0564
Matthew Werny <matthewwerny+github@gmail.com>
2025-03-09 03:38:50 -0600

    Switched to using constant

--- packages/cydran/src/component/MvvmDomWalkerImpl.ts
+++ packages/cydran/src/component/MvvmDomWalkerImpl.ts
@@ -6,14 +6,15 @@ import RegionVisitor from 'component/visitor/RegionVisitor';
 import OtherVisitor from "component/visitor/OtherVisitor";
 import TextVisitor from "component/visitor/TextVisitor";
 import SeriesVisitor from "component/visitor/SeriesVisitor";
+import { TagNames } from "CydranConstants";
 
 class MvvmDomWalkerImpl extends DomWalkerImpl<ComponentInternals> {
 
 	constructor(textVisitor: TextVisitor, otherVisitor: OtherVisitor) {
 		super(otherVisitor, textVisitor, new NonOpVisitor());
-		this.addVisitor("c-component-styles", new ComponentStylesVisitor());
-		this.addVisitor("c-region", new RegionVisitor());
-		this.addVisitor("c-series", new SeriesVisitor());
+		this.addVisitor(TagNames.CYDRAN_COMPONENT_STYLES, new ComponentStylesVisitor());
+		this.addVisitor(TagNames.CYDRAN_REGION, new RegionVisitor());
+		this.addVisitor(TagNames.CYDRAN_SERIES, new SeriesVisitor());
 	}
 
 }
