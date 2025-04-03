28dccfee7041aafa651a644f053783ae7689ee0c
Matthew Werny <matthewwerny+github@gmail.com>
2025-04-03 17:57:16 -0600

    Moved to testing manual recursive delete

--- packages/build/src/task/CleanTask.ts
+++ packages/build/src/task/CleanTask.ts
@@ -1,5 +1,45 @@
 import AbstractTask from "./AbstractTask";
-import { native } from "rimraf";
+
+import fs from 'fs/promises';
+import { existsSync } from 'fs';
+
+async function removeDir(directory) {
+	if (!existsSync(directory)) {
+		return;
+	}
+
+	try {
+		await fs.rm(directory, { recursive: true, force: true });
+	} catch (err) {
+		if (err.code === 'ENOTEMPTY' || err.code === 'EPERM' || err.code === 'EBUSY') {
+			await deleteFilesRecursively(directory);
+		} else {
+			throw err;
+		}
+	}
+}
+
+async function deleteFilesRecursively(directory) {
+	try {
+		const files = await fs.readdir(directory);
+		await Promise.all(files.map(async (file) => {
+			const filePath = `${directory}/${file}`;
+			const stat = await fs.lstat(filePath);
+
+			if (stat.isDirectory()) {
+				await deleteFilesRecursively(filePath);
+				console.log(`Deleting directory: ${filePath}`);
+				await fs.rmdir(filePath);
+			} else {
+				console.log(`Deleting file: ${filePath}`);
+				await fs.unlink(filePath);
+			}
+		}));
+		await fs.rmdir(directory);
+	} catch (err) {
+		console.error(`Failed to delete ${directory}:`, err);
+	}
+}
 
 class CleanTask extends AbstractTask<any> {
 
@@ -14,7 +54,7 @@ class CleanTask extends AbstractTask<any> {
 
 	private async remove(path: string): Promise<void> {
 		this.print("Removed: " + path);
-		await native(path, { glob: false });
+		await removeDir(path);
 	}
 
 }
