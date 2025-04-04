205552c7d4a97e7941116c3197933d10871a1fbc
Matthew Werny <matthewwerny+github@gmail.com>
2025-04-03 18:09:03 -0600

    Moved back to original delete methodology and added debug output

--- packages/build/src/task/CleanTask.ts
+++ packages/build/src/task/CleanTask.ts
@@ -1,45 +1,5 @@
 import AbstractTask from "./AbstractTask";
-
-import fs from 'fs/promises';
-import { existsSync } from 'fs';
-
-async function removeDir(directory) {
-	if (!existsSync(directory)) {
-		return;
-	}
-
-	try {
-		await fs.rm(directory, { recursive: true, force: true });
-	} catch (err) {
-		if (err.code === 'ENOTEMPTY' || err.code === 'EPERM' || err.code === 'EBUSY') {
-			await deleteFilesRecursively(directory);
-		} else {
-			throw err;
-		}
-	}
-}
-
-async function deleteFilesRecursively(directory) {
-	try {
-		const files = await fs.readdir(directory);
-		await Promise.all(files.map(async (file) => {
-			const filePath = `${directory}/${file}`;
-			const stat = await fs.lstat(filePath);
-
-			if (stat.isDirectory()) {
-				await deleteFilesRecursively(filePath);
-				console.log(`Deleting directory: ${filePath}`);
-				await fs.rmdir(filePath);
-			} else {
-				console.log(`Deleting file: ${filePath}`);
-				await fs.unlink(filePath);
-			}
-		}));
-		await fs.rmdir(directory);
-	} catch (err) {
-		console.error(`Failed to delete ${directory}:`, err);
-	}
-}
+import fs from "fs";
 
 class CleanTask extends AbstractTask<any> {
 
@@ -47,14 +7,14 @@ class CleanTask extends AbstractTask<any> {
 		super("Clean");
 	}
 
-	public async execute() {
-		await this.remove(this.getConfig().getCommon().getDistPath());
-		await this.remove(this.getConfig().getCommon().getWorkPath());
+	public async execute(): Promise<void> {
+		this.remove(this.getConfig().getCommon().getDistPath());
+		this.remove(this.getConfig().getCommon().getWorkPath());
 	}
 
-	private async remove(path: string): Promise<void> {
+	private remove(path: string): void {
 		this.print("Removed: " + path);
-		await removeDir(path);
+		fs.rmSync(path, { recursive: true, force: true});
 	}
 
 }
