4df6486bba97448b979585881d4770d03fbe9c3e
Matthew Werny <matthewwerny+github@gmail.com>
2025-08-02 02:02:36 -0600

    Reenabled test

--- packages/integration/test/testcase/digestion/no-behaviors.spec.ts
+++ packages/integration/test/testcase/digestion/no-behaviors.spec.ts
@@ -10,20 +10,47 @@ class TestComponent extends Component {
 
 }
 
+class LoggingSegmentDigester {
+
+	private events: string[];
+
+	constructor() {
+		this.events = [];
+	}
+
+	public digestSegment(id: string, changedCandidates: any[], candidates: any[]): void {
+		for (const candidate of candidates) {
+			let changed: boolean = false;
+
+			try {
+				changed = candidate.evaluate();
+				this.events.push(`Evaluated: ${candidate.getExpression()}`);
+			} catch (e) {
+				throw e;
+			}
+
+			if (changed) {
+				changedCandidates.push(candidate);
+			}
+		}
+	}
+
+	public getEvents(): string[] {
+		return this.events;
+	}
+
+}
+
 describe("No Behaviors", () => {
 
-	test.skip("Digestion - No behaviors", () => {
+	test("Digestion - No behaviors", () => {
 		document.body.innerHTML = '<div></div>';
-
-		const segmentDigester: any = null; // LoggingSegmentDigester = new LoggingSegmentDigester();
-
-		const harness: Harness<TestComponent> = new Harness<TestComponent>(() => new TestComponent(), {
-			"cydran.internal.factory.segment-digester": () => segmentDigester
-		});
+		const harness: Harness<TestComponent> = new Harness<TestComponent>(() => new TestComponent());
+		harness.registerSingleton("cydranSegmentDigester", LoggingSegmentDigester);
 
 		harness.start();
-
-		expect(harness.getDocument().body.innerHTML).toEqual("<div>Hello World!</div>");
+		const segmentDigester: LoggingSegmentDigester = harness.getContext().getObject("cydranSegmentDigester");
+		expect(harness.getDocument().body.innerHTML).toEqual("<!--SS--><!--SE--><div>Hello World!</div><!--SS--><!--SE-->");
 		expect(segmentDigester.getEvents().length).toEqual(0);
 	});
 
