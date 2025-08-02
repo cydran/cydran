8b9a03f9a420a5e7a81d1c99835bf4b110662b3f
Matthew Werny <matthewwerny+github@gmail.com>
2025-08-02 00:29:12 -0600

    Corrected tests

--- packages/cydran/test/stage/StageComponent.spec.ts
+++ packages/cydran/test/stage/StageComponent.spec.ts
@@ -1,13 +1,16 @@
 import StageComponent from "stage/StageComponent";
 import { describe, expect, test } from '@jest/globals';
 import GlobalContextImpl from "context/GlobalContextImpl";
+import Renderer from 'component/Renderer';
+import StageRendererImpl from 'component/renderer/StageRendererImpl';
 
 expect(GlobalContextImpl).not.toBeNull();
 
 describe("StageComponent", () => {
 
 	test("instantiation and whole", () => {
-		const specimen: StageComponent = new StageComponent("<div></div>");
+		const renderer: Renderer = new StageRendererImpl("body");
+		const specimen: StageComponent = new StageComponent(renderer);
 		expect(specimen).not.toBeNull();
 	});
 
--- packages/cydran/test/util/CloneEquals.spec.ts
+++
@@ -1,9 +0,0 @@
-import { cloneDeep, isEqual } from "util/CloneEquals";
-
-test.skip("cloneDeep", () => {
-	// TODO: flesh out the unit
-});
-
-test.skip("isEquals", () => {
-	// TODO: flesh out the unit
-});
