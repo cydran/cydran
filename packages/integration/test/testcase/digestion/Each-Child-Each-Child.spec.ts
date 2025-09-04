8a95bbdd85ae877281c93da5cf67f29ff5623637
Matthew Werny <matthewwerny+github@gmail.com>
2025-09-04 01:03:43 -0600

    Restored Each-Child-Each-Child test

--- packages/integration/test/testcase/digestion/Each-Child-Each-Child.spec.ts
+++ packages/integration/test/testcase/digestion/Each-Child-Each-Child.spec.ts
@@ -1,6 +1,7 @@
 import { Component } from "@cydran/cydran";
 import { Harness } from "@cydran/testsupport";
 import { describe, expect, test } from '@jest/globals';
+import LoggingSegmentDigester from "./LoggingSegmentDigester";
 
 const TEMPLATE: string = `<div>
 	<p data-testid="parent">{{m().items[0].items[0].value}}</p>
@@ -63,14 +64,12 @@ class TestComponent extends Component {
 
 describe("Disconnected Region -> Parent", () => {
 
-	test.skip("Each -> Child -> Disconnected Region -> Child", () => {
-		const segmentDigester: any = null; // LoggingSegmentDigester = new LoggingSegmentDigester();
-
-		const harness: Harness<TestComponent> = new Harness<TestComponent>(() => new TestComponent(), {
-			"cydran.internal.factory.segment-digester": () => segmentDigester
-		});
+	test("Each -> Child -> Disconnected Region -> Child", () => {
+		const harness: Harness<TestComponent> = new Harness<TestComponent>(() => new TestComponent());
+		harness.registerSingletonGlobally("cydranSegmentDigester", LoggingSegmentDigester);
 
 		harness.start();
+		const segmentDigester: LoggingSegmentDigester = harness.getContext().getObject("cydranSegmentDigester");
 
 		harness.forTestId("parent").expect().textContent().toEqual("Alpha");
 		harness.forTestId("child").expect().textContent().toEqual("Alpha");
@@ -79,18 +78,18 @@ describe("Disconnected Region -> Parent", () => {
 		harness.forTestId("child").expect().textContent().toEqual("Beta");
 
 		expect(segmentDigester.getEvents()).toEqual([
-			'0-0-2 - Evaluating - m().items',
-			'0-0-2 - Changed - m().items',
-			'0-0-2 - Evaluating - m().items[0].items[0].value',
-			'0-0-2 - Changed - m().items[0].items[0].value',
-			'0-0-6 - Evaluating - v().items',
-			'0-0-6 - Changed - v().items',
-			'0-0-8 - Evaluating - v().value',
-			'0-0-8 - Changed - v().value',
-			'0-0-2 - Evaluating - m().items',
-			'0-0-2 - Evaluating - m().items[0].items[0].value',
-			'0-0-6 - Evaluating - v().items',
-			'0-0-8 - Evaluating - v().value'
+			"0-0-6 - Evaluating - m().items",
+			"0-0-6 - Changed - m().items",
+			"0-0-6 - Evaluating - m().items[0].items[0].value",
+			"0-0-6 - Changed - m().items[0].items[0].value",
+ 			"0-0-11 - Evaluating - v().items",
+			"0-0-11 - Changed - v().items",
+			"0-0-14 - Evaluating - v().value",
+			"0-0-14 - Changed - v().value",
+			"0-0-6 - Evaluating - m().items",
+			"0-0-6 - Evaluating - m().items[0].items[0].value",
+			"0-0-11 - Evaluating - v().items",
+			"0-0-14 - Evaluating - v().value"
 		]);
 	});
 
